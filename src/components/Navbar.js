aca08747357e09353a83efb3a60cb53505d12237
Scott Morales <morales.scott@gmail.com>
2019-04-24 22:27:09 -0500

    Remove Contact forms example page's link

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -55,14 +55,11 @@ const Navbar = class extends React.Component {
         <Link className="navbar-item" to="/contact">
           Contact
         </Link>
-        <Link className="navbar-item" to="/contact/examples">
-          Form Examples
-        </Link>
       </div>
       <div className="navbar-end has-text-centered">
         <a
           className="navbar-item"
-          href="https://github.com/AustinGreen/gatsby-netlify-cms-boilerplate"
+          href="https://github.com/scottmo223/scott-landing-site"
           target="_blank"
           rel="noopener noreferrer"
         >
