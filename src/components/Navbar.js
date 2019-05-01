4310b3642d3a9299e5ea7c1175a33999a9e855c0
Scott Morales <morales.scott@gmail.com>
2019-05-01 11:37:20 -0500

    comment out products link

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -35,7 +35,7 @@ const Navbar = class extends React.Component {
     <div className="container">
       <div className="navbar-brand">
         <Link to="/" className="navbar-item" title="Logo">
-          <img src={logo} alt="Kaldi" style={{ width: '88px' }} />
+          <img src={logo} alt="Scott Morales" style={{ width: '100px' }} />
         </Link>
         {/* Hamburger menu */}
         <div className="navbar-burger burger" data-target="navMenu">
@@ -49,9 +49,9 @@ const Navbar = class extends React.Component {
         <Link className="navbar-item" to="/about">
           About
         </Link>
-        <Link className="navbar-item" to="/products">
+        {/* <Link className="navbar-item" to="/products">
           Products
-        </Link>
+        </Link> */}
         <Link className="navbar-item" to="/contact">
           Contact
         </Link>
